de214cb86f6404b73eea54355b1e0f5b0e477cae
Obilarius <git@walzenbach.com>
2018-07-20 15:01:52 +0200

    autodruck und Label erstellt

--- script/func.js
+++ script/func.js
@@ -1,4 +1,4 @@
-function getDatum() {
+function getDatumLang() {
   let tage = ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa', 'So'];
 
   let formattedDate = new Date();
@@ -14,5 +14,19 @@ function getDatum() {
 
   let retString = dayOfWeek + " " + date + "." + m + "." + y
 
+  return retString;
+}
+
+function getDatum() {
+  let formattedDate = new Date();
+  let d = formattedDate.getDate();
+  let m =  formattedDate.getMonth();
+  m += 1;  // JavaScript months are 0-11
+  let y = formattedDate.getFullYear();
+
+  if(d < 10){ d = '0' + d; }
+  if(m   < 10){ m   = '0' + m; }
+
+  let retString = d + "." + m + "." + y
   return retString;
 }
